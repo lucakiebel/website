c683b109e4fcc2dc90eaa6380bc4bc7fe0bdab42
lucakiebel <lucakiebel@xenoncloud.net>
2021-07-28 10:39:33 +0200

    Change project images

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'Warframe',
     description: "Warframe is a free-to-play cooperative third-person shooter video game developed by Digital Extremes. In Warframe, players control members of the Tenno, a race of ancient warriors who have awoken from centuries of cryosleep to find themselves at war with different factions.",
-    image: 'https://content.luca-kiebel.de/websites/luca-kiebel.de/img/Warframe.png',
+    image: 'https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/warframe.png',
     tags: ["C#.NET", 'PHP', 'Lua'],
     source: '',
     visit: 'https://warframe.com'
@@ -10,7 +10,7 @@ export const projects = [
   {
     title: "Star Wars Error Pages",
     description: "Star Wars Error Pages are a fun, user friendly and smart way to show your customers that they went down a path that you can't follow, that these are not the sites they were looking for or some other bad Star Wars themed pun.",
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/img/SW-Error.jpeg",
+    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/sw-error.png",
     tags: ["PHP", "JS", "ApacheConf"],
     source: "https://github.com/lucakiebel/star-wars-error-js",
     visit: "https://luca-kiebel.de/sw-demo"
@@ -18,14 +18,14 @@ export const projects = [
   {
     title: "HBBK API",
     description: "An API to interface with the not-so-user-friendly timetable application my school uses. Exploits some bugs in the software to grant \"teacher access\" to more detailed timetables.",
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/img/hbbk_logo.png",
+    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/hbbk.png",
     tags: ["PHP", "School-Project"],
     source: "https://github.com/lucakiebel/HBBK_API",
   },
   {
     title: "LamPi",
     description: "A team of students from Luca's school developed OSS (Open Source Software) under various project names, all Raspberry Pis as a base and present the projects at their school and to visitors of the OpenRheinRuhr in Oberhausen! Luca's team's project is based around moving small vehicles through obstacle courses and controlling them with your smartphone.",
-    image: "https://content.luca-kiebel.de/websites/rpi-interactive/logo.png",
+    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/lampi.png",
     tags: ["NodeJS", "Python", "School-Project"],
     source: "https://github.com/lucakiebel/lampi"
   },
@@ -34,12 +34,12 @@ export const projects = [
     description: "ListX is a free tool to create and maintain Lists for every occasion. Whether it's for your monthly groceries or for the next big vacation - we've got you covered!",
     source: "https://github.com/lucakiebel/ListX",
     tags: ["NodeJS", "angular.js"],
-    image: "https://content.luca-kiebel.de/websites/listx.io/images/favicon.png"
+    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/listx.gif"
   },
   {
     title: "Rocketeers Webshop",
     description: "Webshop for Rocketeers GbR, a German company selling original, hawaiian-made Hawaii shirts.",
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/img/rocketeers.png",
+    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/rocketeers.png",
     tags: ["PHP", "Shopware"],
     visit: "https://rocketeers.de"
   },
@@ -48,7 +48,7 @@ export const projects = [
     description: "Warframe.js is a Wrapper for Warframe's WorldState API. You can utilize Warframe.js to add a widget to your website for any public data from Warframe. ",
     source: "https://github.com/lucakiebel/Warframe.js",
     tags: ["NodeJS", "FetchAPI"],
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/img/Warframe.js.logo.png"
+    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/wf.js.png"
   },
   {
     title: "Nibyou",
@@ -56,7 +56,7 @@ export const projects = [
     source: "https://github.com/nibyou",
     visit: "https://nibyou.com",
     tags: ["NodeJS", "MongoDB", "E2EE"],
-    image: "https://content.luca-kiebel.de/websites/nibyou.de/img/NIBYOU_icon_-_digital.png"
+    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/nibyou.png"
   }
 ];
 
@@ -64,6 +64,6 @@ export const TimeLineData = [
   { year: 2017, text: 'First Software Development Internship at Westfälische Provinzial Versicherung AG'},
   { year: 2018, text: 'Abitur in Maths and Computer Science. Started Studying CS at Clausthal Institute of Technology', },
   { year: 2019, text: 'Began working for XenonCloud GmbH & Co. KG', },
-  { year: 2020, text: 'Projects with three big players in Engeneering and Network Hardware', },
+  { year: 2020, text: 'Projects with three big players in Engineering and Network Hardware', },
   { year: 2021, text: 'Became CTO at Nibyou. Switched University to University of Hagen.', },
 ];
