462393e2c5030b03f8ffc86595cbde71181c0c9d
luca.kiebel <luca.kiebel@adesso.de>
2026-02-10 22:14:17 +0100

    change from old content.luca-kiebel.net to new s3.kiebel.net

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'Warframe',
     description: "Warframe is a free-to-play cooperative third-person shooter video game developed by Digital Extremes. In Warframe, players control members of the Tenno, a race of ancient warriors who have awoken from centuries of cryosleep to find themselves at war with different factions.",
-    image: 'https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/warframe.png',
+    image: 'https://s3.kiebel.net/public/luca-kiebel.de/projectimages/warframe.png',
     tags: ["C#.NET", 'PHP', 'Lua'],
     source: '',
     visit: 'https://warframe.com',
@@ -11,7 +11,7 @@ export const projects = [
   {
     title: "Star Wars Error Pages",
     description: "Star Wars Error Pages are a fun, user friendly and smart way to show your customers that they went down a path that you can't follow, that these are not the sites they were looking for or some other bad Star Wars themed pun.",
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/sw-error.png",
+    image: "https://s3.kiebel.net/public/luca-kiebel.de/projectimages/sw-error.png",
     tags: ["PHP", "JS", "ApacheConf"],
     source: "https://github.com/lucakiebel/star-wars-error-js",
     visit: "https://luca-kiebel.de/sw-demo",
@@ -20,7 +20,7 @@ export const projects = [
   {
     title: "HBBK API",
     description: "An API to interface with the not-so-user-friendly timetable application my school uses. Exploits some bugs in the software to grant \"teacher access\" to more detailed timetables.",
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/hbbk.jpg",
+    image: "https://s3.kiebel.net/public/luca-kiebel.de/projectimages/hbbk.jpg",
     tags: ["PHP", "School-Project"],
     source: "https://github.com/lucakiebel/HBBK_API",
     position: ["Developer"]
@@ -28,7 +28,7 @@ export const projects = [
   {
     title: "LamPi",
     description: "A team of students from Luca's school developed OSS (Open Source Software) under various project names, all Raspberry Pis as a base and present the projects at their school and to visitors of the OpenRheinRuhr in Oberhausen! Luca's team's project is based around moving small vehicles through obstacle courses and controlling them with your smartphone.",
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/lampi.jpg",
+    image: "https://s3.kiebel.net/public/luca-kiebel.de/projectimages/lampi.jpg",
     tags: ["NodeJS", "Python", "School-Project"],
     source: "https://github.com/lucakiebel/lampi",
     position: ["Server Stack Developer"]
@@ -39,13 +39,13 @@ export const projects = [
     source: "https://github.com/lucakiebel/ListX",
     visit:"https://web.archive.org/web/20180902045940/https://listx.io",
     tags: ["NodeJS", "angular.js"],
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/listx.png",
+    image: "https://s3.kiebel.net/public/luca-kiebel.de/projectimages/listx.png",
     position: ["Developer"]
   },
   {
     title: "Rocketeers Webshop",
     description: "Webshop for Rocketeers GbR, a German company selling original, hawaiian-made Hawaii shirts.",
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/rocketeers.png",
+    image: "https://s3.kiebel.net/public/luca-kiebel.de/projectimages/rocketeers.png",
     tags: ["PHP", "Shopware"],
     visit: "https://rocketeers.de",
     position: ["Consultant", "Developer"]
@@ -55,7 +55,7 @@ export const projects = [
     description: "Warframe.js is a Wrapper for Warframe's WorldState API. You can utilize Warframe.js to add a widget to your website for any public data from Warframe. ",
     source: "https://github.com/lucakiebel/Warframe.js",
     tags: ["NodeJS", "FetchAPI"],
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/wf.js.png",
+    image: "https://s3.kiebel.net/public/luca-kiebel.de/projectimages/wf.js.png",
     position: ["Developer"]
   },
   {
@@ -63,7 +63,7 @@ export const projects = [
     description: "Remote controlled car that returns to the starting point at the press of a button.",
     source: "https://github.com/lucakiebel/szAuto",
     tags: ["Python", "C", "Assembler", "Atmega2560"],
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/szauto.jpg",
+    image: "https://s3.kiebel.net/public/luca-kiebel.de/projectimages/szauto.jpg",
     position: ["Developer", "Student"]
   },
   {
@@ -71,7 +71,7 @@ export const projects = [
     description: "jwtauth.go is a simple, yet powerful, JWT authentication server for Go. It's designed to be used in a single-server environment, but can be used in a multi-server environment as well.",
     source: "https://github.com/lucakiebel/jwtauth.go",
     tags: ["Go", "JWT", "microservice"],
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/jwt.jpg",
+    image: "https://s3.kiebel.net/public/luca-kiebel.de/projectimages/jwt.jpg",
     position: ["Developer"]
   },
   {
@@ -79,7 +79,7 @@ export const projects = [
     description: "RSA Public Key Server for key exchange used for symmetric encrypted communication.",
     source: "https://github.com/nibyou/pki",
     tags: ["NodeJS", "microservice"],
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/pki.jpg",
+    image: "https://s3.kiebel.net/public/luca-kiebel.de/projectimages/pki.jpg",
     position: ["CTO", "Developer"]
   },
   {
@@ -88,14 +88,14 @@ export const projects = [
     source: "https://github.com/nibyou",
     visit: "https://nibyou.com",
     tags: ["NodeJS", "PostgreSQL", "E2EE", "Cloudflare Workers"],
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/nibyou.png",
+    image: "https://s3.kiebel.net/public/luca-kiebel.de/projectimages/nibyou.png",
     position: ["CTO", "Co-Founder"]
   },
    {
      title: "adesso Financial Platform",
      description: "The adesso financial platform (afp) is the platform for the financial sector. The solution is based on state-of-the-art software architecture and offers extensive options to optimally design core functions and processes for users. Ready-made building blocks enable individual ‘out-of-the-box’ use.",
      visit: "https://www.adesso.de/en/adesso-branch-solutions/banken-finanzdienstleister/leistungen/adesso-financial-platform/index.jsp",
-     image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/afp.jpg",
+     image: "https://s3.kiebel.net/public/luca-kiebel.de/projectimages/afp.jpg",
      position:["Software Developer"],
      tags: ["Java", "Spring", "eclipse-rcp"]
    },
@@ -103,7 +103,7 @@ export const projects = [
     title: "Run this place",
     description: "Run this place is a comprehensive and smart digital office ecosystem by Union Investment Retail Digital - from finding a parking space to organizing lunch breaks to booking rooms: classic work routines are digitized and simplified by the app.",
     visit: "https://run-this-place.com",
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/rtp.jpg",
+    image: "https://s3.kiebel.net/public/luca-kiebel.de/projectimages/rtp.jpg",
     position: ["Software Engineer (via adesso)"],
     tags: ["NestJS", "PostgreSQL", "GraphQL"]
   },
@@ -111,7 +111,7 @@ export const projects = [
         title: "Audoora Accounting",
         description: 'Audoora Accounting is a service in the Audoora Console that handles client subscriptions for other services, including the creation of invoices, handling subscription revewals and making automatic payments through SEPA direct debit.',
         visit: "https://audoora.de",
-        image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/a-accounting.png",
+        image: "https://s3.kiebel.net/public/luca-kiebel.de/projectimages/a-accounting.png",
         position: ["Software Engineer"],
         tags: ["NestJS", "PostgreSQL", "Vue.js", "SEPA"]
     },
@@ -119,7 +119,7 @@ export const projects = [
         title: "Audoora E-Invoices",
         description: 'Audoora E-Invoices is a customer-facing service in the Audoora Console that allows our customers to receive EU-mandated electronic invoices in Faktur-X/ZUGFeRD/EN-16931 formats and handles automatic invoice creation for audoora\'s Group Purchasing Organisation partners.',
         visit: 'https://audoora.de',
-        image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/a-invoices.png",
+        image: "https://s3.kiebel.net/public/luca-kiebel.de/projectimages/a-invoices.png",
         position: ["Software Engineer"],
         tags: ["NestJS", "PostgreSQL", "Vue.js", "EN-16931"]
     },
@@ -127,7 +127,7 @@ export const projects = [
     title: "Audoora KPI-Dashboard",
     description: "Audiologists' ERP Systems allow for collecting a lot of data on the patients, but don't have any good ways of displaying the collected data to stakeholders. Audoora KPI-Dashboard is a service in the Audoora Console that allows for just that, KPI-analytics for the major ERP systems.",
     visit: "https://audoora.de",
-    image: "https://content.luca-kiebel.de/websites/luca-kiebel.de/projectimages/a-kpi.png",
+    image: "https://s3.kiebel.net/public/luca-kiebel.de/projectimages/a-kpi.png",
     position: ["Software Engineer"],
     tags: ["NestJS", "PostgreSQL", "Vue.js", "MQTT"]
   }
